5da04e73d4264b4bfbbdcd4a0dce750ece17e7a9
Kamiel <de.islaurbana@gmail.com>
2024-01-29 20:34:36 -0600

    updates

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
 //the app.use are called in order with a reason
 app.use(express.static('dist')) //to include the frontend code that is in the dist file
 
@@ -90,10 +86,18 @@ app.get('/api/persons', (request, response) => {
 //     }
 
 // })
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response,next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person){
+                response.json(person)
+            }
+            else{
+                response.status(404).end()
+            }
+            
+        })
+        .catch(error => next(error))
 })
 
 // app.get('/info', (request, response) => {
@@ -105,7 +109,7 @@ app.get('/api/persons/:id', (request, response) => {
 app.get('/info', (request, response) => {
     Person.find({})
         .then(result => {
-            console.log(result)
+            //console.log(result)
             response.send(
                 `<p>The phonebook has info for ${result.length} people</p>
                 <p>${new Date()}</p>`
@@ -113,11 +117,16 @@ app.get('/info', (request, response) => {
         })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    //console.log(persons)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
+    // //console.log(persons)
+    // response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // const generateId = () => {
@@ -152,12 +161,12 @@ app.delete('/api/persons/:id', (request, response) => {
 //     response.json(person)
 // })
 
-app.post('/api/persons', (request,response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+    // if (body.name === undefined || body.number === undefined) {
+    //     return response.status(400).json({ error: 'content missing' })
+    // } -> validation is handeled by the schema (person.js)
 
     const person = new Person({
         name: body.name,
@@ -167,9 +176,46 @@ app.post('/api/persons', (request,response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
+})
+app.put('/api/persons/:id', (request,response,next) => {
+    const body = request.body
+    console.log(body)
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id,person,{new : true})
+        .then(update => {
+            response.json(update)
+        })
+        .catch(error => next(error))
 })
 
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+//unknown endpeoint should be 1 to last app.use
 app.use(unknownEndpoint)
+
+//gets data from the next(error) in the try catch blocks
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if(error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+//error should always be the last app.use
+app.use(errorHandler)
 
 const PORT = process.env.PORT //Fly.io and Render configure the application port based on that environment variable
 app.listen(PORT, () => {
